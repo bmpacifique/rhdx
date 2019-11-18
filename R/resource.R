b5c7cf94544fbc7ceb9e42da5d55dee85f8fafe9
Pacifique BISIMWA MUGISHO <pacifiquemugisho@gmail.com>
2019-11-18 23:25:52 +0100

    Update resource.R

--- R/resource.R
+++ R/resource.R
@@ -55,7 +55,7 @@ Resource <- R6::R6Class(
         dir.create(folder)
 
       if (is.null(filename)) {
-        filename <- basename(file.path(self$data$url)) ## self$data$url
+        filename <- basename(file.path("MonFichier")) ## self$data$url
         if (!is.null(self$data$resource_type) && self$data$resource_type == "api")
           filename <- gsub("\\?.*", "", filename)
       }
